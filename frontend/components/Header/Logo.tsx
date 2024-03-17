8aec96c307a913644b3296760a61aaa2697c86e6
MERITPL\20M531 <bartosz.kowal@merit-automotive.com>
2024-03-17 16:32:15 +0100

    #5 develop basic logo

--- frontend/components/Header/Logo.tsx
+++ frontend/components/Header/Logo.tsx
@@ -1,7 +1,25 @@
-import { Text } from "@chakra-ui/react";
+import { colors } from "@/constants/colors";
+import { Box, Heading } from "@chakra-ui/react";
 
 const Logo: React.FC = () => {
-  return <Text>Logo</Text>;
+  return (
+    <Box backgroundColor={colors.red}>
+      <Heading as="h1" textAlign="center" fontSize="50px" letterSpacing="5px">
+        Crypto
+        <span style={{ fontStyle: "italic", color: colors.yellow }}>Pulse</span>
+      </Heading>
+      <Heading
+        as="h3"
+        textAlign="center"
+        fontSize="20px"
+        letterSpacing="2px"
+        padding="10px"
+        color={colors.bright}
+      >
+        your cryptocurrencies tracker
+      </Heading>
+    </Box>
+  );
 };
 
 export default Logo;
