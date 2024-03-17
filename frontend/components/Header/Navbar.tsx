aa40cd59d4b19b4d5a913b39fa51e81d8410a848
MERITPL\20M531 <bartosz.kowal@merit-automotive.com>
2024-03-17 19:44:47 +0100

    #4 develop large size navigation bar

--- frontend/components/Header/Navbar.tsx
+++ frontend/components/Header/Navbar.tsx
@@ -1,7 +1,43 @@
-import { Text } from "@chakra-ui/react";
+import { colors } from "@/constants/colors";
+import { Box, HStack } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Navbar: React.FC = () => {
-  return <Text>Navbar</Text>;
+  const infoBoxStyles = { px: "15px", borderRight: `2px solid ${colors.dark}` };
+  const largeMenuBoxStyles = {
+    _hover: { transform: "scale(1.1)" },
+    transition: "transform 0.15s",
+  };
+
+  return (
+    <HStack
+      backgroundColor={colors.yellow}
+      justify="space-between"
+      px="20px"
+      py="10px"
+    >
+      <HStack>
+        <Box {...infoBoxStyles}>Market Cap: $2.60T</Box>
+        <Box {...infoBoxStyles}>BTC Dominance: 51.9%</Box>
+        <Box {...infoBoxStyles}>ETH Dominance: 16.8%</Box>
+        <Box {...infoBoxStyles}>Halving: 33D</Box>
+      </HStack>
+      <HStack gap="25px" fontWeight="bold">
+        <Box {...largeMenuBoxStyles}>
+          <Link href="/auth">login/signup</Link>
+        </Box>
+        <Box {...largeMenuBoxStyles}>
+          <Link href="/portfolio">portfolio</Link>
+        </Box>
+        <Box {...largeMenuBoxStyles}>
+          <Link href="/watchlist">watchlist</Link>
+        </Box>
+        <Box {...largeMenuBoxStyles}>
+          <Link href="/tools">tools</Link>
+        </Box>
+      </HStack>
+    </HStack>
+  );
 };
 
 export default Navbar;
