77a593ad4708499f759686e48ddf0e63d254fd03
MERITPL\20M531 <bartosz.kowal@merit-automotive.com>
2024-03-21 14:32:28 +0100

    #31 develop basic layout of table settings section

--- frontend/components/HomeTable/HomeTableSettings.tsx
+++ frontend/components/HomeTable/HomeTableSettings.tsx
@@ -1,7 +1,31 @@
-import { Box } from "@chakra-ui/react";
+import { SettingsIcon } from "@chakra-ui/icons";
+import { Box, Flex, Icon, Input, Select } from "@chakra-ui/react";
 
 const HomeTableSettings: React.FC = () => {
-  return <Box>Home Table Settings Component</Box>;
+  return (
+    <Flex
+      width="100%"
+      justify="space-between"
+      align="center"
+      px="20px"
+      margin="auto"
+    >
+      <Box>
+        <Input placeholder="Search for a coin" />
+      </Box>
+      <Flex gap="20px">
+        <Select cursor="pointer">
+          <option value={100}>100</option>
+          <option value={50}>50</option>
+          <option value={20}>20</option>
+        </Select>
+        <Flex gap="5px" align="center" cursor="pointer">
+          <Icon as={SettingsIcon} />
+          <Box>Customize</Box>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default HomeTableSettings;
