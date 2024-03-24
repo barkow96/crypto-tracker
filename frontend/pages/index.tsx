fd476e8f3a2cd60da3f6b3d9ae9c4264df7898d0
MERITPL\20M531 <bartosz.kowal@merit-automotive.com>
2024-03-24 13:42:52 +0100

    #8 add page metadata - title and description

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,17 @@
 import HomeView from "@/views/HomeView";
+import Head from "next/head";
 
 export default function HomePage() {
-  return <HomeView />;
+  return (
+    <>
+      <Head>
+        <title>CryptoPulse</title>
+        <meta
+          name="description"
+          content="Check the current prices, volumes and all you ever need..."
+        />
+      </Head>
+      <HomeView />
+    </>
+  );
 }
