8ab65da6c484a9ba30792fa12b307cdef4a573cb
MERITPL\20M531 <bartosz.kowal@merit-automotive.com>
2024-04-07 10:54:23 +0200

    #19 develop signup endpoint - add check if user already exists

--- backend/express/controllers/signUpController.js
+++ backend/express/controllers/signUpController.js
@@ -7,9 +7,7 @@ const {
 const ERROR_MESSAGE = "Something went wrong with Strapi endpoint...";
 
 const signUp = async (req, res) => {
-  const submittedFormData = req.body;
-  const { email, password1, password2 } = submittedFormData;
-  console.log("Express.js - submitted form data: ", submittedFormData);
+  const { email, password1, password2 } = req.body;
 
   //SERVER-SIDE DATA VALIDATION
   if (
@@ -24,18 +22,36 @@ const signUp = async (req, res) => {
     return;
   }
 
+  //CHECKING IF USER ALREADY EXISTS
+  const existingUsersResponse = await fetch(
+    `${process.env.STRAPI_URL}/api/website-users`
+  );
+  const existingUsers = await existingUsersResponse.json();
+
+  const userAlreadyExists = existingUsers.data.some(
+    (obj) => obj.attributes.email === email
+  );
+
+  if (userAlreadyExists) {
+    res.status(403).json({ message: "This email is already in use!" });
+    return;
+  }
+
   //TRYING TO SEND THE CREDENTIALS TO POSTGRESQL
   const newUser = {
     data: { email: email, password: password1 },
   };
 
-  let response;
+  let insertNewUserResponse;
   try {
-    response = await fetch(`${process.env.STRAPI_URL}/api/website-users`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newUser),
-    });
+    insertNewUserResponse = await fetch(
+      `${process.env.STRAPI_URL}/api/website-users`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newUser),
+      }
+    );
     res.status(201).json({ message: "User has been created!" });
   } catch (error) {
     res.status(500).json({
